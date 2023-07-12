c27719acd7cd2bc070f64226a715ef36744cb77d
Arthur Molnar <arthurmolnar@artsoft.ro>
2023-07-12 09:26:11 +0300

    Initial commit - Add search by name Table

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TableWithSearchComponent } from './table/table.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableWithSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,270 @@
+import { Component } from '@angular/core';
+
+interface Item {
+  name: string;
+  type: string;
+  email: string;
+  phoneNo: string;
+  companyName: string;
+  expanded: boolean;
+  selected: boolean;
+  showButtons?: boolean;
+  children?: Item[];
+}
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableWithSearchComponent {
+  selectAllRows = false;
+  filteredData: Item[] = [];
+  searchQuery: string = '';
+
+  data = [
+  {
+    name: 'Susan Boyle',
+    type: 'person',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: 'Alphabet',
+    address: '169 11th Street, 94103 San Francisco',
+    selected: false,
+    showButtons: false,
+    expanded: false,
+  },
+  {
+    name: 'Marc Johnson',
+    type: 'person',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: 'Alphabet',
+    address: ' 169 11th Street, 94103 San Francisco ads ad ss',
+    selected: false,
+    showButtons: false,
+    expanded: false,
+  },
+  {
+    name: 'PiedPiper',
+    type: 'person',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: 'Alphabet',
+    address: ' 169 11th Street, 94103 San Francisco ads ad ss',
+    children: [
+      {
+        name: 'Children 1',
+        type: 'company',
+        email: '1 - someone@gmail.com',
+        phoneNo: '1 - +1 628 291 2098',
+        companyName: '1 -',
+        address: '1 - 169 11th Street, 94103 San Francisco',
+        children: [
+          {
+            name: 'Children 21',
+            type: 'company',
+            email: '21 - someone@gmail.com',
+            phoneNo: '21 - +1 628 291 2098',
+            companyName: '21 -',
+            address: '21 - 169 11th Street, 94103 San Francisco',
+            selected: false,
+            expanded: false,
+          },
+          {
+            name: 'Children 31',
+            type: 'company',
+            email: '31 - someone@gmail.com',
+            phoneNo: '31 - +1 628 291 2098',
+            companyName: '31 -',
+            address: '31 - 169 11th Street, 94103 San Francisco',
+            selected: false,
+            expanded: false,
+          },
+        ],
+        selected: false,
+        expanded: false,
+      },
+      {
+        name: 'Children 2',
+        type: 'company',
+        email: '2 - someone@gmail.com',
+        phoneNo: '2 - +1 628 291 2098',
+        companyName: '2 -',
+        address: '2 - 169 11th Street, 94103 San Francisco',
+        children: [
+          {
+            name: 'Children 21',
+            type: 'company',
+            email: '21 - someone@gmail.com',
+            phoneNo: '21 - +1 628 291 2098',
+            companyName: '21 -',
+            address: '21 - 169 11th Street, 94103 San Francisco',
+            selected: false,
+            expanded: false,
+          },
+          {
+            name: 'Children 31',
+            type: 'company',
+            email: '31 - someone@gmail.com',
+            phoneNo: '31 - +1 628 291 2098',
+            companyName: '31 -',
+            address: '31 - 169 11th Street, 94103 San Francisco',
+            selected: false,
+            expanded: false,
+          },
+        ],
+        selected: false,
+        expanded: false,
+      },
+      {
+        name: 'Children 3',
+        type: 'company',
+        email: '3 - someone@gmail.com',
+        phoneNo: '3 - +1 628 291 2098',
+        companyName: '3 -',
+        address: '3 - 169 11th Street, 94103 San Francisco',
+        selected: false,
+        expanded: false,
+      },
+    ],
+    selected: false,
+    showButtons: false,
+    expanded: false
+  },
+  {
+    name: 'PiedPiper',
+    type: 'company',
+    email: 'someone_at_very_lonng_long_long@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: '',
+    address: '169 11th Street, 94103 San Francisco',
+    selected: false,
+    showButtons: false,
+    expanded: false,
+  },
+  {
+    name: 'PiedPiper',
+    type: 'company',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: '',
+    address: '169 11th Street, 94103 San Francisco',
+    children: [
+      {
+        name: 'Children 21',
+        type: 'company',
+        email: '21 - someone@gmail.com',
+        phoneNo: '21 - +1 628 291 2098',
+        companyName: '21 -',
+        address: '21 - 169 11th Street, 94103 San Francisco',
+        selected: false,
+        expanded: false,
+      },
+      {
+        name: 'Children 31',
+        type: 'company',
+        email: '31 - someone@gmail.com',
+        phoneNo: '31 - +1 628 291 2098',
+        companyName: '31 -',
+        address: '31 - 169 11th Street, 94103 San Francisco',
+        selected: false,
+        expanded: false,
+      },
+    ],
+    selected: false ,
+    showButtons: false,
+    expanded: false
+  },
+  {
+    name: 'PiedPiper',
+    type: 'person',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: 'Alphabet',
+    address: '169 11th Street, 94103 San Francisco',
+    selected: false ,
+    showButtons: false,
+    expanded: false,
+  },
+  {
+    name: 'Florin Galan',
+    type: 'person',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: 'Alphabet',
+    address: '169 11th Street, 94103 San Francisco',
+    selected: false,
+    showButtons: false,
+    expanded: false,
+  },
+  {
+    name: 'March Andreesen',
+    type: 'person',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: 'Alphabet',
+    address: '169 11th Street, 94103 San Francisco',
+    children: [
+      {
+        name: 'Andreesen',
+        type: 'company',
+        email: '21 - someone@gmail.com',
+        phoneNo: '21 - +1 628 291 2098',
+        companyName: '21 -',
+        address: '21 - 169 11th Street, 94103 San Francisco',
+        selected: false,
+        expanded: false,
+      },
+      {
+        name: 'Space X',
+        type: 'company',
+        email: '31 - someone@gmail.com',
+        phoneNo: '31 - +1 628 291 2098',
+        companyName: '31 -',
+        address: '31 - 169 11th Street, 94103 San Francisco',
+        selected: false,
+        expanded: false,
+      },
+    ],
+    selected: false,
+    showButtons: false,
+    expanded: false,
+  },
+  {
+    name: 'Ben Horowitz',
+    type: 'person',
+    email: 'someone@gmail.com',
+    phoneNo: '+1 628 291 2098',
+    companyName: 'Alphabet',
+    address: '169 11th Street, 94103 San Francisco',
+    selected: false,
+    showButtons: false,
+    expanded: false,
+  },
+];
+
+constructor() {
+  this.filteredData = this.data;
+}
+
+  filterTable() {
+    this.filteredData = this.data.filter(item =>
+      item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  toggleSelectAll() {
+    for (let person of this.filteredData) {
+      person.selected = this.selectAllRows;
+    }
+  }
+
+  toggleRow(item: any) {
+    item.expanded = !item.expanded;
+  }
+
+  selectedRowCount(): number {
+    return this.data.filter(item => item.selected).length;
+  }
+}
